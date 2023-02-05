853ad57bc2389324c815af0678a9eea8d5c1c6cb
Ra_v <Ra_v@DESKTOP-OU9TR15>
2023-02-05 18:33:11 +0100

    Add project files.

---
+++ HelloWorld11/math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld11
+{
+    public class Math
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
